ce651723515d84c3c74dd7fa189cd1079e129e3d
Marcelo Rosende <marcelo.rosende@softvision.com>
2019-05-10 12:20:01 -0300

    First Push

---
+++ tst/HomeControllerShould.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using Take2.Source.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tst
+{
+    public class HomeControllerShould
+    {
+        HomeController _sut;
+        public HomeControllerShould()
+        {
+            _sut = new HomeController();
+        }
+        [Fact]
+        public void ShowHome()
+        {
+            var result = _sut.Index();
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
